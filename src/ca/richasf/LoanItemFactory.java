76f7904c2f51f406c485f6a0c1ae664ea25aef89
Richard Gao <hga38@sfu.ca>
2026-06-18 00:02:06 -0700

    use map because we need key set anyways

--- src/ca/richasf/LoanItemFactory.java
+++ src/ca/richasf/LoanItemFactory.java
@@ -1,5 +1,9 @@
 package ca.richasf;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Supplier;
+
 import ca.richasf.model.AudioLoanItem;
 import ca.richasf.model.BookLoanItem;
 import ca.richasf.model.LoanItem;
@@ -9,15 +13,13 @@ import ca.richasf.model.MovieLoanItem;
  * A factory that prompts the user to create loan items.
  */
 public final class LoanItemFactory {
-    public static final String BOOK = "book", AUDIO = "audio", MOVIE = "movie";
+    private static final Map<String, Supplier<LoanItem>> PROMPTS = Map.of(
+                "book", LoanItemFactory::promptBookLoanItem,
+                "audio", LoanItemFactory::promptAudioLoanItem,
+                "movie", LoanItemFactory::promptMovieLoanItem);
 
-    public static LoanItem getInstance(String type) throws UnknownLoanItemTypeException {
-        return switch (type) {
-            case BOOK -> promptBookLoanItem();
-            case AUDIO -> promptAudioLoanItem();
-            case MOVIE -> promptMovieLoanItem();
-            default -> throw new UnknownLoanItemTypeException();
-        };
+    public static Optional<LoanItem> getInstance(String type) {
+        return Optional.ofNullable(PROMPTS.get(type)).map(Supplier::get);
     }
 
     private static BookLoanItem promptBookLoanItem() {
